c6c0c9f50ab143fce787a693ddd6475105bbac09
Маринка <marihaxxx@mail.ru>
2015-04-23 22:40:41 +0300

    dz

--- win_api/Dodonova_home.cpp
+++ win_api/Dodonova_home.cpp
@@ -4,7 +4,8 @@
 #include<tchar.h>
 #include<vector>
 #include<iostream>
-#include <cstdio>
+#include<cstdio>
+#include<list>
 using namespace std;
 
 #define ID_BUTTON1 1001
@@ -23,13 +24,15 @@ using namespace std;
 #define ID_CHECK2 51
 #define ID_CHECK3 52
 #define ID_CHECK4 53
+#define ID_COMBO 200
+#define ID_COMBO2 201
 
 static TCHAR WindowsClass[] = "win32app";
 static TCHAR Title[] = "MyWindow";
 HINSTANCE hinst;
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK ChildProc(HWND, UINT, WPARAM, LPARAM);				// для дочернего окна
-HWND add_btn, go_btn, list1, list2, edit, edit_name, edit_salary, edit_num, checkbox1, checkbox2, checkbox3, checkbox4;
+HWND add_btn, go_btn, list1, list2, edit, edit_name, edit_salary, edit_num, checkbox1, checkbox2, checkbox3, checkbox4, combo, combo2;
 int window_y_pos;
 int width = 500, heigth = 500;
 
@@ -77,65 +80,91 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpcmdLine
 	return (int)msg.wParam;
 }
 
-//1. В клиентской области окна приложения создать 2 списка, первый из которых содержит числа от 1 до 5,
-// второй – числа от 200 до 205. При нажатии кнопки «Добавить» в список с фокусом ввода добавить очередное число.
 
-int l = 0, k = 0;
-const int size = 5;
-int f = -1;
-int arr1[size], arr2[size];
-char s[20] = {0};
+// Игра "Убить врага"
 
-void SetArr() {
-	for (int i = 0, k = 200; i < size; i++, k++) {
-		arr1[i] = i + 1;
-		arr2[i] = k;
+char what[20], where[20], result[20];
+int arm[] = { 20, 10, 5 }, leg[] = { 30, 20, 10 };
+int death = 0, line, line1;
+
+void Update(HWND arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		if (SendMessage(arr[i], BM_GETCHECK, 0, 0) == 1) {
+			SendMessage(arr[i], BM_SETCHECK, 0, 0);
+		}
 	}
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	
-
+	HWND enemy[] = { checkbox1, checkbox2, checkbox3 };
 	switch (message) {
 	case WM_COMMAND:
-		if (HIWORD(wParam) == LBN_SETFOCUS) {
-			switch (LOWORD(wParam)) {
-			case ID_LIST1:
-				f = 1;
-				break;
-
-			case ID_LIST2:
-				f = 2;
-				break;
-			}
+		if (LOWORD(wParam) == ID_COMBO && HIWORD(wParam) == CBN_SELENDOK) {
+			line = SendMessage(combo, CB_GETCURSEL, 0, 0);
+			SendMessage(combo, CB_GETLBTEXT, line, (LPARAM)what);
 		}
 
-	case BN_CLICKED:
+		if (LOWORD(wParam) == ID_COMBO2 && HIWORD(wParam) == CBN_SELENDOK) {
+			line1 = SendMessage(combo2, CB_GETCURSEL, 0, 0);
+			SendMessage(combo2, CB_GETLBTEXT, line1, (LPARAM)where);
+		}
+
 		if (LOWORD(wParam) == ID_BUTTON1) {
-			if (f == 1) {
-				if (l >= size) {
-					l = 0;
-				}
-				_itoa_s(arr1[l], s, 10);
-				SendMessage(list1, LB_ADDSTRING, 0, (LPARAM)s);
-				l++;
+			memset(result, 0, sizeof(result));				// обнуляю результат
+			strcat_s(result, what);
+			strcat_s(result, where);
+			SendMessage(list1, LB_ADDSTRING, 0, (LPARAM)result);
+			if (line == 0) {
+				death += arm[line1];
 			}
-			if(f == 2) {
-				if (k >= size) {
-					k = 0;
-				}
-				_itoa_s(arr2[k], s, 10);
-				SendMessage(list2, LB_ADDSTRING, 0, (LPARAM)s);
-				k++;
+			if (line == 1) {
+				death += leg[line1];
 			}
+			Update(enemy, 3);
+
+			switch (line1) {
+			case 0:
+				SendMessage(checkbox1, BM_SETCHECK, 1, 0);
+				break;
+			case 1:
+				SendMessage(checkbox2, BM_SETCHECK, 1, 0);
+				break;
+			case 2:
+				SendMessage(checkbox3, BM_SETCHECK, 1, 0);
+				break;
+			}
+			if (death >= 100) {
+				death = 100;
+				string s = "Смерть: " + std::to_string(death) + "%";
+				SendMessage(edit, WM_SETTEXT, 0, (LPARAM)s.c_str());
+				MessageBox(hwnd,  "Враг убит!", "", TRUE);
+				Update(enemy, 3);
+				death = 0;
+			}
+			string s = "Смерть: " + std::to_string(death) + "%";
+			SendMessage(edit, WM_SETTEXT, 0, (LPARAM)s.c_str());
+			
 		}
 		break;
 
 	case WM_CREATE:
-		add_btn = CreateWindowEx(NULL, "button", "добавить", WS_CHILD | WS_VISIBLE, 180, 250, 100, 20, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
-		list1 = CreateWindowEx(WS_EX_CLIENTEDGE, "listbox", "", WS_CHILD | WS_VISIBLE | LBS_STANDARD, 50, 150, 100, 300, hwnd, (HMENU)ID_LIST1, hinst, NULL);
-		list2 = CreateWindowEx(WS_EX_CLIENTEDGE, "listbox", "", WS_CHILD | WS_VISIBLE | LBS_STANDARD, 300, 150, 100, 300, hwnd, (HMENU)ID_LIST2, hinst, NULL);
-		SetArr();
+		combo = CreateWindowEx(WS_EX_CLIENTEDGE, "combobox", "", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 30, 100, 100, 150, hwnd, (HMENU)ID_COMBO, hinst, NULL);
+		combo2 = CreateWindowEx(WS_EX_CLIENTEDGE, "combobox", "", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 30, 180, 100, 150, hwnd, (HMENU)ID_COMBO2, hinst, NULL);
+		SendMessage(combo, WM_SETTEXT, 0, (LPARAM) "ЧЕМ");
+		SendMessage(combo, CB_ADDSTRING, 0, (LPARAM) "рукой");
+		SendMessage(combo, CB_ADDSTRING, 0, (LPARAM) "ногой");
+
+		SendMessage(combo2, WM_SETTEXT, 0, (LPARAM) "КУДА");
+		SendMessage(combo2, CB_ADDSTRING, 0, (LPARAM) " в голову");
+		SendMessage(combo2, CB_ADDSTRING, 0, (LPARAM) " в живот");
+		SendMessage(combo2, CB_ADDSTRING, 0, (LPARAM) " в ноги");
+
+		add_btn = CreateWindowEx(NULL, "button", "УДАР", WS_CHILD | WS_VISIBLE, 30, 270, 100, 50, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
+		list1 = CreateWindowEx(WS_EX_CLIENTEDGE, "listbox", "", WS_CHILD | WS_VISIBLE, 150, 100, 120, 300, hwnd, (HMENU)ID_LIST1, hinst, NULL);
+		checkbox1 = CreateWindowEx(NULL, "button", "голова", WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 350, 200, 100, 20, hwnd, (HMENU)ID_CHECK1, hinst, NULL);
+		checkbox2 = CreateWindowEx(NULL, "button", "живот", WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 350, 230, 100, 20, hwnd, (HMENU)ID_CHECK2, hinst, NULL);
+		checkbox3 = CreateWindowEx(NULL, "button", "ноги", WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 350, 260, 100, 20, hwnd, (HMENU)ID_CHECK3, hinst, NULL);
+		edit = CreateWindowEx(NULL, "edit", "Смерть: ", WS_CHILD | WS_VISIBLE | ES_READONLY, 350, 350, 100, 20, hwnd, (HMENU)ID_EDIT1, hinst, NULL);
 		break;
 
 	case WM_DESTROY:
@@ -150,92 +179,120 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 
 }
 
-
-//2. На форме разместить 4 элемента Check Box c надписями «Сильный», «Смелый», «Добрый», «Умный».
-// При нажатии на кнопку «Добавить» выбранные качества отображаются с использованием MessageBox. 
-
-//LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-//	char str[50] = {0};
-//	char buf[50];
-//	HWND checkboxes[] = { checkbox1, checkbox2, checkbox3, checkbox4 };
-//	int size = 4;
-//	LRESULT res;
-//
-//	switch (message) {
-//
-//	case WM_COMMAND:
-//		for (int i = 0; i < size; i++) {
-//			res = SendMessage(checkboxes[i], BM_GETCHECK, 0, 0);
-//			if (res == BST_CHECKED) {
-//				GetWindowText(checkboxes[i], buf, 50);
-//				strcat_s(str, sizeof(str), buf);
-//				strcat_s(str, " ");
-//			}
-//		}
-//
-//		case BN_CLICKED:
-//			if (LOWORD(wParam) == ID_BUTTON1) {
-//				MessageBox(hwnd, str, "", TRUE);
-//			}
-//
-//		break;
-//
-//	case WM_CREATE:
-//		add_btn = CreateWindowEx(NULL, "button", "добавить", WS_CHILD | WS_VISIBLE, 180, 250, 100, 20, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
-//		checkbox1 = CreateWindowEx(NULL, "button", "Сильный", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 200, 40, 100, 20, hwnd, (HMENU)ID_CHECK1, hinst, NULL);
-//		checkbox2 = CreateWindowEx(NULL, "button", "Смелый", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 200, 70, 100, 20, hwnd, (HMENU)ID_CHECK2, hinst, NULL);
-//		checkbox3 = CreateWindowEx(NULL, "button", "Добрый", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 200, 100, 100, 20, hwnd, (HMENU)ID_CHECK3, hinst, NULL);
-//		checkbox4 = CreateWindowEx(NULL, "button", "Умный", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 200, 130, 100, 20, hwnd, (HMENU)ID_CHECK4, hinst, NULL);
-//		break;
-//
-//	case WM_DESTROY:
-//		PostQuitMessage(0);
-//		break;
-//
-//	default:
-//		return DefWindowProc(hwnd, message, wParam, lParam);
-//	}
-//
-//	return 0;
-//
-//}
+//###########################################################################################################
 
 
+//1. Комбинированный список заполнить произвольными различными строками. Рядом расположить кнопку «Вниз». 
+// Если выбрать строку и нажать кнопку, то эта строка должна переместиться вниз на одну позицию в списке.
+// Если эта строка уже последняя в списке, то сообщить об этом и заблокировать кнопку. 
 
-// 3. На форме разместить список строк, однострочный редактор и кнопка «Найти».
-// При нажатии на кнопку найти строку из редактора в списке строк.Если найти не удалось, то сообщить об этом.
-
-//string words[] = { "синий", "белый", "красный", "черный", "зеленый", "серый", "розовый", "желтый" };
-//int size = 8;
-//
-//void SetList() {
+//string words[] = { "Побег из Шоушенка", "Зеленая миля", "Форрест Гамп", "Список Шиндлера", "1+1", "Король Лев", "Начало", "Леон", "Интерстеллар", "Бойцовский клуб" };
+//void SetList(HWND list, int size) {
 //	for (int i = 0; i < size; i++) {
-//		SendMessage(list1, LB_ADDSTRING, 0, (LPARAM)words[i].c_str());
+//		SendMessage(list, CB_ADDSTRING, 0, (LPARAM)words[i].c_str());
 //	}
 //}
-//
+//int size = 10,  cur_sel;
 //
 //LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-//	char str[50];
-//
+//	string str, tmp;
+//		
 //	switch (message) {
-//
 //	case WM_COMMAND:
+//		if (LOWORD(wParam) == ID_COMBO && HIWORD(wParam) == CBN_SELENDOK) {
+//			cur_sel = SendMessage(combo, CB_GETCURSEL, 0, 0);
+//		}
+//	
 //		if (LOWORD(wParam) == ID_BUTTON1) {
-//			SendMessage(edit, WM_GETTEXT, sizeof(str), (LPARAM)str);
-//			auto res = SendMessage(list1, LB_SELECTSTRING, 1, (LPARAM)str);
+//			int length = SendMessage(combo, CB_GETCOUNT, 0, 0);
+//			if (cur_sel == length - 1) {
+//				MessageBox(hwnd, "Это последняя строка!", "", TRUE);
+//			}
+//			else {
+//				SendMessage(combo, CB_GETLBTEXT, cur_sel + 1, (LPARAM)tmp.c_str());
+//				SendMessage(combo, CB_GETLBTEXT, cur_sel, (LPARAM)str.c_str());
 //
-//			if (res == -1) {
-//				MessageBox(hwnd, "Нет такой строки!", 0, 0);
+//				SendMessage(combo, CB_DELETESTRING, cur_sel, 0);
+//				SendMessage(combo, CB_INSERTSTRING, cur_sel + 1, (LPARAM)str.c_str());
 //			}
 //		}
+//	
+//		break;
+//	case BN_CLICKED:
+//	
+//		break;
+//		
+//	case WM_CREATE:
+//		combo = CreateWindowEx(WS_EX_CLIENTEDGE, "combobox", "", WS_CHILD | WS_VISIBLE, 30, 100, 200, 300, hwnd, (HMENU)ID_COMBO, hinst, NULL);
+//		SetList(combo, size);
+//				
+//		add_btn = CreateWindowEx(NULL, "button", "Вниз", WS_CHILD | WS_VISIBLE, 300, 270, 100, 20, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
+//		break;
+//		
+//	case WM_DESTROY:
+//		PostQuitMessage(0);
+//		break;
+//		
+//	default:
+//		return DefWindowProc(hwnd, message, wParam, lParam);
+//	}
+//		
+//	return 0;
+//		
+//}
+
+//######################################################################################################################################
+
+//2. Отобразить комбинированный список, содержащий названия специальностей, однострочный редактор с надписью «Фамилия»
+// и кнопку «Записать». При нажатии кнопки конкатенация строки из редактора и названия выбранной специальности 
+// записывается в список строк.  
+
+//string jobs[] = { "продавец", "менеджер", "сварщик", "программист", "дизайнер", "администратор", "рекламщик", "копирайтер", "верстальщик", "секретарь" };
+//int size = 10, cur_sel;
+//
+//void SetList(HWND list) {
+//	for (int i = 0; i < size; i++) {
+//		SendMessage(list, CB_ADDSTRING, 0, (LPARAM)jobs[i].c_str());
+//	}
+//}
+//LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+//	char str[20], name[20], result[20];
+//	HDC hdc;
+//	PAINTSTRUCT ps;
+//
+//	switch (message) {
+//	case WM_COMMAND:
+//		if (LOWORD(wParam) == ID_COMBO && HIWORD(wParam) == CBN_SELENDOK) {
+//			cur_sel = SendMessage(combo, CB_GETCURSEL, 0, 0);
+//		}
+//
+//		if (LOWORD(wParam) == ID_BUTTON1) {
+//			SendMessage(combo, CB_GETLBTEXT, cur_sel, (LPARAM)str);
+//			SendMessage(edit, WM_GETTEXT, 20, (LPARAM)name);
+//			if (name[0] == 0) {
+//				MessageBox(hwnd, "Нет фамилии!", "", TRUE);
+//			}
+//			else {
+//				memset(result, 0, sizeof(result));
+//				strcat_s(result, name);
+//				strcat_s(result, " ");
+//				strcat_s(result, str);
+//				SendMessage(list1, LB_ADDSTRING, 0, (LPARAM)result);
+//			}
+//		}
+//
+//		break;
+//	case WM_PAINT:
+//		hdc = BeginPaint(hwnd, &ps);
+//		TextOut(hdc, 190, 80, "Фамилия:", 8);
 //		break;
 //
 //	case WM_CREATE:
-//		add_btn = CreateWindowEx(NULL, "button", "Найти", WS_CHILD | WS_VISIBLE, 180, 250, 100, 20, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
-//		edit =  CreateWindowEx(NULL, "edit", "", WS_CHILD | WS_VISIBLE, 180, 200, 100, 20, hwnd, (HMENU)ID_EDIT1, hinst, NULL);
-//		list1 = CreateWindowEx(WS_EX_CLIENTEDGE, "listbox", "", WS_CHILD | WS_VISIBLE | LBS_STANDARD, 50, 150, 100, 300, hwnd, (HMENU)ID_LIST1, hinst, NULL);
-//		SetList();
+//		combo = CreateWindowEx(WS_EX_CLIENTEDGE, "combobox", "", WS_CHILD | WS_VISIBLE, 30, 100, 150, 200, hwnd, (HMENU)ID_COMBO, hinst, NULL);
+//		edit = CreateWindowEx(NULL, "edit", "", WS_CHILD | WS_VISIBLE, 190, 100, 100, 20, hwnd, (HMENU)ID_EDIT1, hinst, NULL);
+//		add_btn = CreateWindowEx(NULL, "button", "Записать", WS_CHILD | WS_VISIBLE, 190, 270, 100, 20, hwnd, (HMENU)ID_BUTTON1, hinst, NULL);
+//		list1 = CreateWindowEx(WS_EX_CLIENTEDGE, "listbox", "name_list", WS_CHILD | WS_VISIBLE, 300, 100, 150, 195, hwnd, (HMENU)ID_LIST1, hinst, NULL);
+//		SetList(combo);
 //		break;
 //
 //	case WM_DESTROY:
@@ -249,6 +306,3 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 //	return 0;
 //
 //}
-
-
-
